2e4912d1357802e73d0ee29ad7e796531d5ddb40
bkobeng <obengstoney@gmail.cpm>
2016-08-07 22:42:07 +0000

    profile imagWe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-   root 'listings#index'
-   resources :listings
+  root 'listings#index'
+  resources :listings
  get 'signup', to:'users#new'
   resources :users, except: [:new]
 
